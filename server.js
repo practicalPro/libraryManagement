786103aac6303c57bb337018d9e07ca718ab5a74
Yen-Ting (Andy) Chou <103315214+practicalPro@users.noreply.github.com>
2024-06-19 19:03:20 +0800

    update package.json and add server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+// Feature (at least 5 different pages with different "routing"):
+// Add Book
+// Edit Book
+// Delete Book
+// View All Books List
+// Display Book Details
+// You can use mockup data in JSON format
+// If you can, create backend using Node.js (Better) / Firebase
+// Push the code to your PUBLIC github repository.
+
+
+// Importing express module
+const express = require('express');
+
+
+const app = express();
+
+
